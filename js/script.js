470f1e4ffca9ad244fd7ee6b1b126b9511dd2f52
Eliza Śpionek <eliza.spionek@gmail.com>
2021-05-05 21:13:36 +0200

    js changed

--- js/script.js
+++ js/script.js
@@ -1,26 +1,34 @@
-let form = document.querySelector(".js-form");
-let plnElement = document.querySelector(".js-pln");
-let eurElement = document.querySelector(".js-eur");
-let usdElement = document.querySelector(".js-usd");
-let gbpElement = document.querySelector(".js-gbp");
+{
+    const form = document.querySelector(".js-form");
+    const plnElement = document.querySelector(".js-pln");
+    const eurElement = document.querySelector(".js-eur");
+    const usdElement = document.querySelector(".js-usd");
+    const gbpElement = document.querySelector(".js-gbp");
+    
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const rateEUR = 4.56;
+    const rateUSD = 3.77;
+    const rateGBP = 5.23;
 
-    let plnValue = plnElement.value;
-    let eurValue = plnValue / 4.56;
-    let usdValue = plnValue / 3.77;
-    let gbpValue = plnValue / 5.23;
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+    
+        const plnValue = plnElement.value;
+        const eurValue = plnValue / rateEUR;
+        const usdValue = plnValue / rateUSD;
+        const gbpValue = plnValue / rateGBP;
+    
+        eurElement.innerText = eurValue.toFixed(2) + " EUR";
+        usdElement.innerText = usdValue.toFixed(2) + " USD";
+        gbpElement.innerText = gbpValue.toFixed(2) + " GBP";
+    
+    });
+    
+    form.addEventListener("reset", () => {
+    
+        eurElement.innerText = "0 EUR";
+        usdElement.innerText = "0 USD";
+        gbpElement.innerText = "0 GBP";
+    })
 
-    eurElement.innerText = eurValue.toFixed(2) + " EUR";
-    usdElement.innerText = usdValue.toFixed(2) + " USD";
-    gbpElement.innerText = gbpValue.toFixed(2) + " GBP";
-
-});
-
-form.addEventListener("reset", () => {
-
-    eurElement.innerText = "0 EUR";
-    usdElement.innerText = "0 USD";
-    gbpElement.innerText = "0 GBP";
-})
+}
