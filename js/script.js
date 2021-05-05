26ab6023a146a34fdd5e202378c00adc014190c6
Eliza Śpionek <eliza.spionek@gmail.com>
2021-05-05 21:57:26 +0200

    js changed

--- js/script.js
+++ js/script.js
@@ -1,34 +1,45 @@
 {
-    const form = document.querySelector(".js-form");
-    const plnElement = document.querySelector(".js-pln");
+
+
     const eurElement = document.querySelector(".js-eur");
     const usdElement = document.querySelector(".js-usd");
     const gbpElement = document.querySelector(".js-gbp");
     
 
-    const rateEUR = 4.56;
-    const rateUSD = 3.77;
-    const rateGBP = 5.23;
+    const showResult = (eurValue, usdValue, gbpValue ) => {
+        eurElement.innerText = eurValue.toFixed(2) + " EUR";
+        usdElement.innerText = usdValue.toFixed(2) + " USD";
+        gbpElement.innerText = gbpValue.toFixed(2) + " GBP";
+       
+    }
 
-    form.addEventListener("submit", (event) => {
+    const submit = (event) => {
+        const plnElement = document.querySelector(".js-pln");
         event.preventDefault();
-    
+       
+        const rateEUR = 4.56;
+        const rateUSD = 3.77;
+        const rateGBP = 5.23;
+
         const plnValue = plnElement.value;
         const eurValue = plnValue / rateEUR;
         const usdValue = plnValue / rateUSD;
         const gbpValue = plnValue / rateGBP;
-    
-        eurElement.innerText = eurValue.toFixed(2) + " EUR";
-        usdElement.innerText = usdValue.toFixed(2) + " USD";
-        gbpElement.innerText = gbpValue.toFixed(2) + " GBP";
-    
-    });
-    
-    form.addEventListener("reset", () => {
-    
+        showResult(eurValue, usdValue, gbpValue);
+    }
+
+    const resetForm = () => {
         eurElement.innerText = "0 EUR";
         usdElement.innerText = "0 USD";
         gbpElement.innerText = "0 GBP";
-    })
+    }
+    const init = () => {
+        const form = document.querySelector(".js-form");
+        const resetButton = document.querySelector(".js-resetButton");
+        form.addEventListener("submit", submit);
+        resetButton.addEventListener("reset", resetForm);
+    }
+init();
 
 }
+
